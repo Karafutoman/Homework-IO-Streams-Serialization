56f0bd48225970c36b04690f3ba83a9921a250ba
Andrey <109068333+Karafutoman@users.noreply.github.com>
2022-09-12 10:06:02 +1100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,12 @@ import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) throws Exception {
-
         String[] products = {"Кофе", "Сыр", "Колбаса", "Хлеб", "Кефир"};
         int[] price = {500, 220, 450, 50, 80};
 
         Basket basket = new Basket(products, price);
 
         while (true) {
-
             Scanner scanner = new Scanner(System.in);
             System.out.println("Список возможных товаров для покупки:");
 
@@ -20,14 +18,15 @@ class Main {
 
             String input = scanner.nextLine();
             if (input.equals("end")) {
-                basket.saveTxt(new File("basket.txt"));
+                // basket.saveTxt(new File("basket.txt"));
+                basket.saveBin(new File("basket.bin"));
                 System.out.println("Программа завершена!");
                 break;
             }
-
             int indexProduct = Integer.parseInt(String.valueOf(input.charAt(0))) - 1;
             int amountProduct = Integer.parseInt(String.valueOf(input.charAt(2)));
             basket.addToCart(indexProduct, amountProduct);
+
         }
         basket.printCart();
     }
